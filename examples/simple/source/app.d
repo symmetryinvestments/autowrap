21355a76ef33516a502b976fd584f3ceacc0a06b
Atila Neves <atila.neves@gmail.com>
2018-08-14 12:30:06 +0200

    Remove configurations from main autowrap dub.sdl

--- examples/simple/source/app.d
+++ examples/simple/source/app.d
@@ -1,8 +1,11 @@
 import autowrap.python;
+
 mixin(
-    wrapAll(LibraryName("simple"),
-            Modules("prefix", "adder", "structs", "templates", "api",
-                    Module("wrap_all", Yes.alwaysExport)
-            ),
-        )
-    );
+    wrapAll(
+        LibraryName("simple"),
+        Modules(
+            "prefix", "adder", "structs", "templates", "api",
+            Module("wrap_all", Yes.alwaysExport)
+        ),
+    )
+);
