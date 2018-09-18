a32eb672d964a1beff610b19ea8ecc15f71752c6
Adam Wilson <adamw@ellipticbit.com>
2018-09-18 00:46:18 -0700

    Added missing RIDs.

--- csharp/library/build_linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -f libautowrap.*.so
-dub build --arch=x86 --force
-mv libautowrap-support.so libautowrap.x86.so
-dub build --arch=x86_64 --force
-mv libautowrap-support.so libautowrap.x64.so
-dotnet build
-dotnet pack
