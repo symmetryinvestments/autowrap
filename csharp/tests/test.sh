ca8dece2829d270991f04b7461caf7329f9d1526
Adam Wilson <adamw@ellipticbit.com>
2018-12-04 02:12:13 -0800

    Add C# tests to Travis. Clean up C# project and build scripts.

--- csharp/tests/test.sh
+++ csharp/tests/test.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+CWD=$PWD
 
+cd $DIR
 rm -f csharp-tests
 rm -f libcsharp*.so
-dub build --arch=x86 --force
-mv libcsharp-tests.so libcsharp-tests.x86.so
 dub build --arch=x86_64 --force > /dev/null 2>&1
 mv libcsharp-tests.so libcsharp-tests.x64.so
 rm -f libcsharp-tests.so
 dub run --config=emitCSharp
 dotnet build
 LD_LIBRARY_PATH=$DIR dotnet vstest ./bin/Debug/netcoreapp2.1/tests.dll
+cd $CWD
