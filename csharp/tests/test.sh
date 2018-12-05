0432f708f070a6aa2868ddde50e7057ffd6b7113
Adam Wilson <adamw@ellipticbit.com>
2018-12-04 16:18:48 -0800

    Remove rm commands.

--- csharp/tests/test.sh
+++ csharp/tests/test.sh
@@ -4,11 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 CWD=$PWD
 
 cd $DIR
-rm -f csharp-tests
-rm -f libcsharp*.so
 dub build --arch=x86_64 --force > /dev/null 2>&1
 mv libcsharp-tests.so libcsharp-tests.x64.so
-rm -f libcsharp-tests.so
 dub run --config=emitCSharp
 dotnet build
 LD_LIBRARY_PATH=$DIR dotnet test
