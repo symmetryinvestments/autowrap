7964a6fc334d7ddd7358e8328e35d9345724be06
Atila Neves <atila.neves@gmail.com>
2018-12-05 15:54:50 +0100

    Remove dead code

--- pynih/source/python/type.d
+++ pynih/source/python/type.d
@@ -6,86 +6,14 @@ module python.type;
 import python.raw: PyObject;
 import std.traits: isArray, isIntegral, isBoolean, isFloatingPoint;
 
-/**
-   Creates storage for a `PyTypeObject` for each D type `T`.
- */
-template OldPythonType(T) {
-    import python.raw: PyObject, PyTypeObject, PyMemberDef;
-    import std.traits: Fields;
-
-    private PyTypeObject _pyType;
-    private PyMemberDef[Fields!T.length + 1] members;
-
-    void init() {
-        import python.raw: MemberType, TypeFlags, PyType_GenericNew;
-        import std.traits: Fields, FieldNameTuple;
-
-        if(_pyType != _pyType.init) return;
-
-        _pyType.tp_name = &__traits(identifier, T)[0];
-        _pyType.tp_basicsize = (PythonAggregate!T).sizeof;
-        _pyType.tp_flags = TypeFlags.Default;  // this is important for Python2
-        _pyType.tp_new = &PyType_GenericNew;
-        _pyType.tp_init = &ctor;
-        _pyType.tp_repr = &repr;
-        _pyType.tp_str = &repr;
-
-        // TODO: members
-        // static foreach(i; 0 .. Fields!T.length) {
-        //     members[i].name = cast(typeof(PyMemberDef.name)) &FieldNameTuple!T[i][0];
-        //     members[i].type = MemberType.Int; // FIXME
-        //     members[i].offset = __traits(getMember, T, FieldNameTuple!T[i]).offsetof + PythonAggregate!T.original.offsetof;
-        // }
-        // _pyType.tp_members = &members[0];
-
-        // TODO: methods
-    }
-
-    PyObject* pyObject() {
-        init;
-        return cast(PyObject*) &_pyType;
-    }
-
-    PyTypeObject* pyType() {
-        init;
-        return &_pyType;
-    }
-
-    extern(C) static PyObject* repr(PyObject* self_) {
-        import python: pyUnicodeDecodeUTF8;
-        import std.conv: text;
-
-        assert(self_ !is null);
-        auto self = cast(PythonAggregate!T*) self_;
-        auto ret = text(self.original);
-        return pyUnicodeDecodeUTF8(ret.ptr, ret.length, null /*errors*/);
-    }
-
-    extern(C) static int ctor(PyObject* self_, PyObject* args, PyObject* kwargs) {
-        assert(self_ !is null);
-        auto self = cast(PythonAggregate!T*) self_;
-        self.original = T();
-        // TODO: arguments
-        return 0;
-    }
-}
-
-
 
 /**
-   A Python extension type for a D aggregate T.
+   A wrapper for `PyTypeObject`.
+
+   This struct does all of the necessary boilerplate to intialise
+   a `PyTypeObject` for a Python extension type that mimics the D
+   type `T`.
  */
-struct PythonAggregate(T) {
-    import python.raw: PyObjectHead;
-
-    alias Type = T;
-
-    mixin PyObjectHead;
-    T original;
-    alias original this;
-}
-
-
 struct PythonType(T) {
     import python.raw: PyTypeObject;
     import std.traits: FieldNameTuple, Fields;
@@ -112,21 +40,19 @@ struct PythonType(T) {
 
         if(_pyType != _pyType.init) return;
 
-        if(_pyType == _pyType.init) {
-            _pyType.tp_name = T.stringof;
-            _pyType.tp_basicsize = PythonClass!T.sizeof;
-            _pyType.tp_flags = TypeFlags.Default;
-            _pyType.tp_new = &PyType_GenericNew;
-            _pyType.tp_getset = getsetDefs;
-            _pyType.tp_methods = methodDefs;
-            _pyType.tp_repr = &repr;
-            _pyType.tp_init = &init;
-            _pyType.tp_new = &new_;
+        _pyType.tp_name = T.stringof;
+        _pyType.tp_basicsize = PythonClass!T.sizeof;
+        _pyType.tp_flags = TypeFlags.Default;
+        _pyType.tp_new = &PyType_GenericNew;
+        _pyType.tp_getset = getsetDefs;
+        _pyType.tp_methods = methodDefs;
+        _pyType.tp_repr = &repr;
+        _pyType.tp_init = &init;
+        _pyType.tp_new = &new_;
 
-            if(PyType_Ready(&_pyType) < 0) {
-                PyErr_SetString(PyExc_TypeError, &"not ready"[0]);
-                failedToReady = true;
-            }
+        if(PyType_Ready(&_pyType) < 0) {
+            PyErr_SetString(PyExc_TypeError, &"not ready"[0]);
+            failedToReady = true;
         }
     }
 
