54c49703b39c6316ee87f86bd8b7668ec8e1f8a9
Adam Wilson <adamw@ellipticbit.com>
2018-12-05 00:38:36 -0800

    Add travis debugging output.

--- csharp/tests/test.sh
+++ csharp/tests/test.sh
@@ -4,7 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 CWD=$PWD
 
 cd $DIR
-echo $CWD
 echo $PWD
 echo $DIR/bin/Debug/netcoreapp2.1
 dub build --arch=x86_64 --force > /dev/null 2>&1
@@ -12,13 +11,11 @@ cp libcsharp-tests.so libcsharp-tests.x64.so
 dub run --config=emitCSharp
 dotnet build
 
-ls -la $CWD
 ls -la $PWD
 ls -la $DIR/bin/Debug/netcoreapp2.1
 
 LD_LIBRARY_PATH=$DIR/bin/Debug/netcoreapp2.1 dotnet test
 
-ls -la $CWD
 ls -la $PWD
 ls -la $DIR/bin/Debug/netcoreapp2.1
 
