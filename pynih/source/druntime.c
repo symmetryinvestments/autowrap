e7e2c49a1c0a31b33befdc21ec5547d3af4ad674
Atila Neves <atila.neves@gmail.com>
2018-12-07 19:18:58 +0100

    Rename init and term functions in pynih

--- pynih/source/druntime.c
+++ pynih/source/druntime.c
@@ -5,9 +5,9 @@
 extern int rt_init(void);
 extern int rt_term(void);
 
-__attribute__((__constructor__)) void dinit(void) {
+__attribute__((__constructor__)) void dinit_pynih(void) {
     rt_init();
 }
-__attribute__((__destructor__)) void dterm(void) {
+__attribute__((__destructor__)) void dterm_pynih(void) {
     rt_term();
 }
