2c3d43416114e8331136b2f66cf4c87c1b0c1646
Laeeth Isharc <laeeth@kaleidic.io>
2018-12-17 15:48:07 +0000

    csharp should wrap python test too

--- examples/simple/source/app.d
+++ examples/simple/source/app.d
@@ -1,11 +1,30 @@
-import autowrap.python;
+version(WrapPython)
+{
+	import autowrap.python;
 
-mixin(
-    wrapAll(
-        LibraryName("simple"),
-        Modules(
-            "prefix", "adder", "structs", "templates", "api",
-            Module("wrap_all", Yes.alwaysExport)
-        ),
-    )
-);
+	mixin(
+	    wrapAll(
+		LibraryName("simple"),
+		Modules(
+		    "prefix", "adder", "structs", "templates", "api",
+		    Module("wrap_all", Yes.alwaysExport)
+		),
+	    )
+	);
+}
+
+version(WrapCSharp)
+{
+	import autowrap.csharp;
+	mixin(
+	    wrapCSharp(
+		Modules(
+		    "prefix", "adder", "structs", "templates", "api",
+		    Module("wrap_all") // , Yes.alwaysExport),
+		),
+		OutputFileName("simple.cs"),
+		LibraryName("simple"),
+		RootNamespace("simple")
+	    )
+	);
+}
