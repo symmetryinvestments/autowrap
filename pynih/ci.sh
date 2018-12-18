1337f692abab1547a4965303a940b38b60f7e3d0
Atila Neves <atila.neves@gmail.com>
2018-12-18 15:44:08 +0100

    Simplify pynih CI

--- pynih/ci.sh
+++ pynih/ci.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 set -euo pipefail
+
 DC="${DC:-dmd}"
+TRAVIS="${TRAVIS:-}"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 PYTHON_INCLUDE_DIR="${PYTHON_INCLUDE_DIR:-/usr/include/python3.7m}"
 
@@ -11,8 +13,11 @@ if [ "$DC" = "ldc2" ]; then
     exit 0
 fi
 
-cd "$SCRIPT_DIR"/contract
-make
 cd "$SCRIPT_DIR"
-ls
-dub run -c unittest-travis --build=unittest-cov
+make
+
+if [[ -z "${TRAVIS}" ]]; then
+    dub test -q
+else
+    dub run -c unittest-travis --build=unittest-cov
+fi
--- pynih/tests/ut/python/util.d
+++ pynih/tests/ut/python/util.d
@@ -7,6 +7,7 @@ import python.cooked;
 
 @("Aggregates.stringifyTypes with templated struct")
 @safe pure unittest {
+    import python.boilerplate: Aggregates;
     static struct Struct(T) { }
     auto aggs = Aggregates!(Struct!int, Struct!double)();
     aggs.stringifyTypes.should == "Struct!int, Struct!double";
