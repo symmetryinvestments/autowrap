4b5e505baebffa49951a8771cd5e4b380cc3bccd
Atila Neves <atila.neves@gmail.com>
2018-12-18 11:24:11 +0100

    Fix deprecation warning

--- python/source/autowrap/python/package.d
+++ python/source/autowrap/python/package.d
@@ -4,5 +4,5 @@
 module autowrap.python;
 
 public import autowrap.python.boilerplate;
-public import autowrap.reflection: Module;
+public import autowrap.reflection: Modules, Module;
 public import std.typecons: Yes, No;
