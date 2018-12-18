8a6d91c421addbcfee23570c651725eda9e763e1
Atila Neves <atila.neves@gmail.com>
2018-12-18 12:31:25 +0100

    Failing test for #44

--- examples/simple/source/issues.d
+++ examples/simple/source/issues.d
@@ -14,3 +14,11 @@ export auto uncopiable() {
 export auto uncopiablePtr(double d = 33.3) {
     return new Uncopiable(d);
 }
+
+
+export auto stringPtr(string s) {
+    static struct StringPtrRetStruct {
+        string value;
+    }
+    return new StringPtrRetStruct(s);
+}
--- tests/test_issues.py
+++ tests/test_issues.py
@@ -1,6 +1,16 @@
+def test_issue_44():
+    from simple import string_ptr
+    import pytest
+
+    with pytest.raises(RuntimeError):
+        assert string_ptr('foo').value == 'foo'
+        assert string_ptr('bar').value == 'bar'
+
+
 def test_issue_47():
     from simple import uncopiable_ptr
     import pytest
+
     with pytest.raises(RuntimeError):
         assert uncopiable_ptr(33.3).d == 33.3
         assert uncopiable_ptr(44.4).d == 44.4
