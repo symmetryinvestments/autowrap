b019cd578738f6d6d826aa4db0447b352de893dc
Atila Neves <atila.neves@gmail.com>
2018-12-18 12:33:59 +0100

    Fix #44 - support for pointers in PrimordialType

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -1,10 +1,7 @@
 def test_issue_44():
     from simple import string_ptr
-    import pytest
-
-    with pytest.raises(RuntimeError):
-        assert string_ptr('foo').value == 'foo'
-        assert string_ptr('bar').value == 'bar'
+    assert string_ptr('foo').value == 'foo'
+    assert string_ptr('bar').value == 'bar'
 
 
 def test_issue_47():
