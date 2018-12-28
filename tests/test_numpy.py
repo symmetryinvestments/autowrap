0b90fe6aed8a99b906c815e68a39b46256568915
Atila Neves <atila.neves@gmail.com>
2018-12-28 13:35:01 +0000

    numpy int tests

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -4,3 +4,67 @@ def test_bool():
 
     assert if_true_then_42_else_33(bool_(True)) == 42
     assert if_true_then_42_else_33(bool_(False)) == 33
+
+
+def test_int8():
+    from numpytests import add1
+    from numpy import int8
+
+    assert add1(int8(2), int8(3)) == 6
+    assert add1(int8(1), int8(1)) == 3
+
+
+def test_int16():
+    from numpytests import add1
+    from numpy import int16
+
+    assert add1(int16(2), int16(3)) == 6
+    assert add1(int16(1), int16(1)) == 3
+
+
+def test_int32():
+    from numpytests import add1
+    from numpy import int32
+
+    assert add1(int32(2), int32(3)) == 6
+    assert add1(int32(1), int32(1)) == 3
+
+
+def test_int64():
+    from numpytests import add1
+    from numpy import int64
+
+    assert add1(int64(2), int64(3)) == 6
+    assert add1(int64(1), int64(1)) == 3
+
+
+def test_uint8():
+    from numpytests import add1
+    from numpy import uint8
+
+    assert add1(uint8(2), uint8(3)) == 6
+    assert add1(uint8(1), uint8(1)) == 3
+
+
+def test_uint16():
+    from numpytests import add1
+    from numpy import uint16
+
+    assert add1(uint16(2), uint16(3)) == 6
+    assert add1(uint16(1), uint16(1)) == 3
+
+
+def test_uint32():
+    from numpytests import add1
+    from numpy import uint32
+
+    assert add1(uint32(2), uint32(3)) == 6
+    assert add1(uint32(1), uint32(1)) == 3
+
+
+def test_uint64():
+    from numpytests import add1
+    from numpy import uint64
+
+    assert add1(uint64(2), uint64(3)) == 6
+    assert add1(uint64(1), uint64(1)) == 3
