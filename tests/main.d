2f0df90a0848a5c6af0c057dd91cb2ba4dd1da52
Atila Neves <atila.neves@gmail.com>
2019-05-17 18:43:59 +0200

    autowrap ut config depends on pynih

--- tests/main.d
+++ tests/main.d
@@ -1,5 +1,20 @@
 import unit_threaded.runner;
 
+
+import python;
+export __gshared extern(C) PyDateTime_CAPI* PyDateTimeAPI;
+
+
+shared static this() {
+    Py_Initialize;
+    pyDateTimeImport;
+}
+
+shared static ~this() {
+    Py_Finalize;
+}
+
+
 mixin runTestsMain!(
     "autowrap.reflection",
     "autowrap.python.wrap",
