2c949ad6efc12673701798301684eafbdb575c54
Atila Neves <atila.neves@gmail.com>
2019-05-17 18:56:54 +0200

    Delete pynih copies of reflection structs

--- pynih/source/autowrap/python/package.d
+++ pynih/source/autowrap/python/package.d
@@ -2,6 +2,7 @@ module autowrap.python;
 
 
 public import std.typecons: Yes, No;
+public import autowrap.reflection: Modules, Module;
 
 
 /**
@@ -23,48 +24,6 @@ struct PreModuleInitCode {
  */
 struct PostModuleInitCode {
     string value;
-}
-
-
-/**
-   The list of modules to automatically wrap for consumption by other languages.
- */
-struct Modules {
-    import std.traits: Unqual;
-    import std.meta: allSatisfy;
-
-    Module[] value;
-
-    this(A...)(auto ref A modules) {
-
-        foreach(module_; modules) {
-            static if(is(Unqual!(typeof(module_)) == Module))
-                value ~= module_;
-            else static if(is(Unqual!(typeof(module_)) == string))
-                value ~= Module(module_);
-            else
-                static assert(false, "Modules must either be `string` or `Module`");
-        }
-    }
-}
-
-/**
-   A module to automatically wrap.
-   Usually not needed since a string will do, but is useful when trying to export
-   all functions from a module by using Module("mymodule", Yes.alwaysExport)
-   instead of "mymodule"
- */
-struct Module {
-    import std.typecons: Flag, No;
-
-    string name;
-    Flag!"alwaysExport" alwaysExport = No.alwaysExport;
-
-    string toString() @safe pure const {
-        import std.conv: text;
-        import std.string: capitalize;
-        return text(`Module("`, name, `", `, text(alwaysExport).capitalize, `.alwaysExport)`);
-    }
 }
 
 
