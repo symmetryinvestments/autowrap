c89d90c1b2018f619acaa3baa3d68a7b3fc321a9
Atila Neves <atila.neves@gmail.com>
2019-05-31 11:35:55 +0200

    Move contract boilerplate code to its own dub package

---
+++ pynih/contract/source/contract/boilerplate.d
@@ -0,0 +1,86 @@
+module contract.boilerplate;
+
+import python.boilerplate;
+
+/**
+   A string mixin to reduce boilerplate when creating a Python module.
+   Takes a module name and a variadic list of C functions to make
+   available.
+ */
+string createModuleMixin(Module module_, alias cfunctions, alias aggregates)()
+    if(isPython3)
+{
+    if(!__ctfe) return null;
+
+    import std.format: format;
+
+    enum ret = q{
+        import python.raw: PyDateTime_CAPI;
+        // This is declared as an extern C variable in python.bindings.
+        // We declare it here to avoid linker errors.
+        export __gshared extern(C) PyDateTime_CAPI* PyDateTimeAPI;
+
+        import python: ModuleInitRet;
+
+        extern(C) export ModuleInitRet PyInit_%s() {
+            import python.raw: pyDateTimeImport;
+            import python.cooked: createModule;
+            import python.boilerplate: Module, CFunctions, Aggregates;
+            import core.runtime: rt_init;
+
+            rt_init;
+
+            pyDateTimeImport;
+
+            return createModule!(
+                Module("%s"),
+                CFunctions!(
+                    %s
+                ),
+                Aggregates!(
+                    %s
+                )
+            );
+        }
+    }.format(module_.name, module_.name, cfunctions.stringifySymbols, aggregates.stringifyTypes);
+
+    return ret;
+}
+
+string createModuleMixin(Module module_, alias cfunctions, alias aggregates)()
+    if(isPython2)
+{
+    if(!__ctfe) return null;
+
+    import std.format: format;
+
+    enum ret = q{
+        import python.raw: PyDateTime_CAPI;
+
+        // This is declared as an extern C variable in python.bindings.
+        // We declare it here to avoid linker errors.
+        export __gshared extern(C) PyDateTime_CAPI* PyDateTimeAPI;
+
+        extern(C) export void init%s() {
+            import python.raw: pyDateTimeImport;
+            import python.cooked: initModule;
+            import python.boilerplate: Module, CFunctions, Aggregates;
+            import core.runtime: rt_init;
+
+            rt_init;
+
+            pyDateTimeImport;
+            initModule!(
+                Module("%s"),
+                CFunctions!(
+                    %s
+                ),
+                Aggregates!(
+                    %s
+                ),
+            );
+        }
+    }.format(module_.name, module_.name, cfunctions.stringifySymbols, aggregates.stringifyTypes);
+
+    return ret;
+}
