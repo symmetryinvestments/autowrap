7d455d014f2a73737b047dc02c63a66ccac68d81
Atila Neves <atila.neves@gmail.com>
2019-06-02 18:00:37 +0200

    Ignore dpp .tmp files

--- examples/pyd/source/app.d
+++ examples/pyd/source/app.d
@@ -1,8 +1,10 @@
-import autowrap.python;
-import std.typecons: Yes;
+version(Have_autowrap_pynih)
+    import autowrap.pynih;
+else
+    import autowrap.python;
 
-mixin(
-    wrapAll(
+version(Have_autowrap_pynih) {
+    enum str = wrapDlang!(
         LibraryName("pyd"),
         Modules(
             Module("arraytest", Yes.alwaysExport),
@@ -13,5 +15,22 @@ mixin(
             Module("const_", Yes.alwaysExport),
             Module("class_wrap", Yes.alwaysExport),
         ),
-    )
-);
+    );
+    pragma(msg, str);
+    mixin(str);
+} else {
+    mixin(
+        wrapAll(
+            LibraryName("pyd"),
+            Modules(
+                Module("arraytest", Yes.alwaysExport),
+                Module("inherit", Yes.alwaysExport),
+                Module("testdll", Yes.alwaysExport),
+                Module("def", Yes.alwaysExport),
+                Module("struct_wrap", Yes.alwaysExport),
+                Module("const_", Yes.alwaysExport),
+                Module("class_wrap", Yes.alwaysExport),
+            ),
+        )
+    );
+}
