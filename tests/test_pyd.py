647b4279063217c37efdb707f7d9bf84801a9184
Atila Neves <atila.neves@gmail.com>
2019-07-31 17:08:56 +0200

    Fix test_array for pynih due to #54

--- tests/test_pyd.py
+++ tests/test_pyd.py
@@ -9,20 +9,32 @@ def test_array():
     from pyd import Foo, get, set, test
 
     f = Foo(2)
-    # FIXME - see #54
-    with pytest.raises(AttributeError):
+    # the bug only exists for pyd, not pynih
+    if is_pyd:
+        # FIXME - see #54
+        with pytest.raises(AttributeError):
+            assert f.i == 2
+    else:
         assert f.i == 2
 
     assert get() == []
 
     set([Foo(10), Foo(20)])
     assert [f.value() for f in get()] == [10, 20]
-    with pytest.raises(AttributeError):
+
+    if is_pyd:
+        with pytest.raises(AttributeError):  # See #54
+            assert [f.i for f in get()] == [10, 20]
+    else:
         assert [f.i for f in get()] == [10, 20]
 
     t = test()
-    with pytest.raises(AttributeError):
+    if is_pyd:
+        with pytest.raises(AttributeError):  # See #54
+            assert t.i == 10
+    else:
         assert t.i == 10
+
     # bar can't be tested due to side-effects
     t.bar()
     assert str(t) == '{10}'
