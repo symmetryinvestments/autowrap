58108429b28c858a0bdd3e6774296f114a63a164
Atila Neves <atila.neves@gmail.com>
2019-12-04 11:35:14 +0100

    Rename existing phobos test

--- tests/test_phobos.py
+++ tests/test_phobos.py
@@ -5,7 +5,7 @@ is_pyd = os.environ.get('PYD')
 is_pynih = os.environ.get('PYNIH')
 
 
-def test_protocol():
+def test_std_socket_protocol():
     if is_pyd:
         with pytest.raises(ImportError):
             from phobos import Protocol
