1b2598092e2e67fcaaf61931b7be27cb08293fd2
Atila Neves <atila.neves@gmail.com>
2020-01-06 14:00:23 +0100

    Python tests for std.math

--- examples/phobos/tests/test_std_math.py
+++ examples/phobos/tests/test_std_math.py
@@ -1,9 +1,33 @@
 import os
+import pytest
 
 
 is_pyd = os.environ.get('PYD')
 is_pynih = os.environ.get('PYNIH')
 
 
-def test_import():
-    import std_math
+def test_fabs():
+    from std_math import fabs
+    for i in range(10):
+        number = i + 0.1  # turn it into a float
+        assert abs(number) == fabs(number)
+
+
+# FIXME
+def test_sqrt():
+    with pytest.raises(ImportError):
+        from std_math import sqrt
+
+
+def test_ceil():
+    from std_math import ceil
+    assert ceil(0.0) == 0
+    assert ceil(1.234) == 2
+    assert ceil(-1.234) == -1
+
+
+def test_floor():
+    from std_math import floor
+    assert floor(0.0) == 0
+    assert floor(1.234) == 1
+    assert floor(-1.234) == -2
