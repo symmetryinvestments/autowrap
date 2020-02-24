68bbb9689f6ec903a758089fc2fe6dfc95cd9d8a
Atila Neves <atila.neves@gmail.com>
2020-02-24 18:43:27 +0100

    Move all C# tests to the Makefile

--- ci/test-csharp.sh
+++ ci/test-csharp.sh
@@ -3,7 +3,5 @@
 set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo 'Running C# script test'
-"$SCRIPT_DIR"/../csharp/tests/test.sh
 echo 'Running C# makefile test'
 make -j"$(nproc)" -C "$SCRIPT_DIR"/.. test_cs
