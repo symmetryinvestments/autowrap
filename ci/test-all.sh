ad664d3549ee411affa5839660d4e22809f87e65
Atila Neves <atila.neves@gmail.com>
2020-03-03 17:12:45 +0100

    Enable Phobos tests

--- ci/test-all.sh
+++ ci/test-all.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$SCRIPT_DIR"/test-python.sh
 "$SCRIPT_DIR"/test-csharp.sh
-make -j"$(nproc)" -C "$SCRIPT_DIR"/.. test_translation
+make -j"$(nproc)" -C "$SCRIPT_DIR"/.. test_translation test_phobos
