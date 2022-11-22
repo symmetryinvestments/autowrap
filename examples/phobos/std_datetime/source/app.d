df39c48098bf4492039bc9126741f2860f20fe24
Atila Neves <atila.neves@gmail.com>
2022-11-22 17:19:39 -0300

    Fix test_std_datetime

--- examples/phobos/std_datetime/source/app.d
+++ examples/phobos/std_datetime/source/app.d
@@ -27,3 +27,21 @@ private PyObject* hack0(PyObject*, PyObject*, int) { assert(0); }
 
 pragma(mangle, "_D6python4type__T13PythonCompareTS3std8datetime8timezone13PosixTimeZone6TTInfoZ7_py_cmpUNbPSQDm3raw7_objectQriZQv")
 private PyObject* hack1(PyObject*, PyObject*, int) { assert(0); }
+
+import autowrap.pynih.python.type;
+import autowrap.pynih.python.raw;
+import std.typecons;
+import std.datetime.timezone;
+// pragma(mangle, "_D8autowrap5pynih6python4type__T13PythonCompareTS3std8typecons__T10RebindableTyCQBf8datetime8timezone8TimeZoneZQBuZ7_py_cmpUNbPSQEwQEqQEn3raw7_objectQxiZQBb")
+// extern (C) _object* PythonCompare!(Rebindable!(immutable(TimeZone)))._py_cmp(_object*, _object*, int) nothrow {
+//     assert(0);
+// }
+
+void hack2() {
+    Rebindable!(immutable(TimeZone)) reb;
+    auto t = PythonType!(typeof(reb))();
+    auto pyType = t.pyType;
+    auto rich = pyType.tp_richcompare;
+    rich(null, null, 0);
+    pragma(msg, typeof(rich));
+}
