101f44c0e3e36797a3357ba65f741e3cde63022d
Atila Neves <atila.neves@gmail.com>
2026-04-17 13:13:29 -0700

    Limit new compiler CI to core tests

--- ci/test-all-non-pyd.sh
+++ ci/test-all-non-pyd.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-"$SCRIPT_DIR"/../pynih/ci.sh
-make -C "$SCRIPT_DIR"/.. ut test_python_pynih test_translation
+make -C "$SCRIPT_DIR"/.. ut test_translation
